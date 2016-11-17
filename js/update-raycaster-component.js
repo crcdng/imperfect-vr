29ceeaee49499f3787cd2e3612df8fd0bd03f9b8
mike <michael.straeubig@plymouth.ca.uk>
2016-11-17 00:17:30 +0000

    main scene: removed obsolete update raycaster component

--- js/update-raycaster-component.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('update-raycaster', {
-  schema: {
-    type: 'selector'
-  },
-  init: function () {
-     var raycasterEl = this.data;
-     raycasterEl.components.raycaster.refreshObjects();
-  }
-});
