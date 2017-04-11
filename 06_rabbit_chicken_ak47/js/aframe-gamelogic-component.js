5225ee1c0b1f195104edebb65f8a30f39ccf4d0a
mike <straeubig@i3games.de>
2017-04-11 20:32:36 +0100

    rabbit hops

--- 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
+++ 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
@@ -1,6 +1,7 @@
 // this is the complete gamelogic in one component
 // we use event-set to send messages to this component and it handles the logic
-// it is ok for our purpose here it is specific to this scene and not reusable for others
+// it is ok for our purpose here
+// it is specific to this scene and not reusable for others
 // in practice we would separate it out into several smaller components that can be reused
 
 AFRAME.registerComponent('gamelogic', {
@@ -26,21 +27,25 @@ AFRAME.registerComponent('gamelogic', {
 		// when we "click" on the rabbit by looking at it:
 		// we attach the follow component
 		// the "follow me" text gets invisible
+		// the rabbit starts hopping
 		// the rabbit starts moving along its path (alongpath/curve component)
 		if (state === 'follow' && previousState !== 'movingended') {
 			this.followme.setAttribute('visible', false);
 			this.player.setAttribute('follow', 'target', '#avatar');
+			this.rabbit.setAttribute('animation__hop', 'property: position; to: 0 0.3 0; dur: 250; easing: easeInOutSine; loop: true');
 			this.avatar.setAttribute('alongpath', 'curve: '+this.track+'; dur: 3000');
 		}
-		// when the rabbit is done
+		// when the rabbit is done:
 		// we stop following it
+		// the rabbit stops hopping
 		// the rabbit hovers in mid air before falling down
 		// at the end it gets invisible
 		if (state === 'movingended') {
 			if (this.track === '#track1') {
 				this.player.removeAttribute('follow');
-				this.track = '#track2';
 				this.avatar.removeAttribute('alongpath');
+				this.rabbit.removeAttribute('animation__hop');
+				this.track = '#track2';
 				this.avatar.setAttribute('alongpath', 'curve: '+this.track+'; delay: 4000; dur: 3000');
 			} else if (this.track === '#track2') {
 				this.rabbit.setAttribute('visible', false);
