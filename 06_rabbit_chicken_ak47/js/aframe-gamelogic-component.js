d2b63627ddeb9c97b44d82d5a12135420bcb829f
mike <straeubig@i3games.de>
2017-04-24 18:13:00 +0100

    improved platform choice code

--- 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
+++ 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
@@ -1,8 +1,11 @@
 // this is the complete gamelogic in one component
 // we use event-set to send messages to this component and it handles the logic
-// it is ok for our purpose here
-// it is specific to this scene and not reusable for others
-// in practice we would separate it out into several smaller components that can be reused
+// this approach is ok for our purpose here, but note that there are better ones:
+// - the component is specific to this scene and not reusable for others
+// - in practice we would separate it out into several smaller components
+//   that can be reused
+// - there are more advanced JavaScript techniques to handle a bigger scene
+//   I'd like to cver those in the future.
 
 AFRAME.registerComponent('gamelogic', {
 	schema: { // this tells us what goes into the component
@@ -25,8 +28,10 @@ AFRAME.registerComponent('gamelogic', {
 	update: function (oldData) { // this function is called each time when something is updated
 		var raycaster = document.querySelector('[raycaster]').components.raycaster;
 		var state = this.data.state;
+		var parameter;
 		if (oldData === {}) { return; }
 		var previousState = oldData.state;
+		var target;
 		console.log("state: " + state + ", previous state: " + previousState);
 
 		// 1. start of the scene, we follow the rabbit
@@ -72,26 +77,23 @@ AFRAME.registerComponent('gamelogic', {
 				this.el.setAttribute('gamelogic', 'state: heart');
 			}).bind(this));
 			raycaster.refreshObjects();
-		} else if (state === 'ak47') {
-			var start = player.getAttribute('position');
-			var target = this.platform3.getAttribute('position');
-			target.y = target.y + 30;
+		} else if (state === 'ak47' || state === 'heart') {
+			if (state === 'ak47') {
+				target = this.platform3.getAttribute('position');
+				parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: '+target.x+' '+(target.y + 33)+' '+target.z;
+			} else if (state === 'heart') {
+				target = this.platform2.getAttribute('position');
+				parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: '+target.x+' '+(target.y + 36)+' '+target.z;
+			}
 			console.log(target);
-			var parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: '+target.x+' '+target.y+' '+target.z;
+			// stop the animation
 			this.player.removeAttribute('animation');
 			this.player.setAttribute('animation', parameters);
-			this.ak47.setAttribute('visible', false);
-			this.killthebeast.setAttribute('visible', false);
-		} else if (state === 'heart') {
-			var start = player.getAttribute('position');
-			var target = this.platform2.getAttribute('position');
-			target.y = target.y + 36;
-			console.log(target);
-			var parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: '+target.x+' '+target.y+' '+target.z;
-			this.player.removeAttribute('animation');
-			this.player.setAttribute('animation', parameters);
-			this.heart.setAttribute('visible', false);
-			this.loveandpeace.setAttribute('visible', false);
+			// remove all choice elements
+			this.heart.parentNode.removeChild(this.heart);
+			this.ak47.parentNode.removeChild(this.ak47);
+			this.killthebeast.parentNode.removeChild(this.killthebeast);
+			this.loveandpeace.parentNode.removeChild(this.loveandpeace);
 		}
-	}
+	} // end of update()
 });
