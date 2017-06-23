08ace984cbf10266d6215b5d4810a30b923184b5
mike <straeubig@i3games.de>
2017-06-23 01:15:27 +0100

    introducing the move-along component

--- 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
+++ 06_rabbit_chicken_ak47/js/aframe-gamelogic-component.js
@@ -1,9 +1,46 @@
 // this is the complete gamelogic in one component
-// using event-set to send messages to this component and it handles the logic
-// this approach not good for more complex scenes:
-// - the component is specific to this scene and not reusable for others
-// - there are better techniques to handle state logic.
-// I'd like to cover those in the future.
+// the elements in the scene use the event-set component to send messages to it
+// and it handles the logic
+
+// this approach would not work well for more complex scenes
+// (image hundreds of states)
+// there are better techniques to handle state logic
+// also this component is specific to this scene and not reusable for others
+
+// two helper functions
+
+// construct a lerp-ed path with sinusodial movement on y.
+// assume start.y === target.y
+// returns an array of points [{x: x0, y: y0, z: z0}, ...]
+function createJumpPath (startPos, endPos, points = 10, maxHeight = 11.5) {
+  var path = [];
+	var i, step, stepY, x, y, z;
+	if (points == null) { point}
+  for (i = 0; i <= points; i = i + 1) {
+    step = i / points;
+    stepY = (i / points) * Math.PI;
+    x = startPos.x + step * (endPos.x - startPos.x);
+    y = startPos.y + Math.sin(stepY) * maxHeight;
+    z = startPos.z + step * (endPos.z - startPos.z);
+    path.push({x: x, y: y, z: z});
+  }
+  return path;
+}
+
+function createCurveEntityFromJumpPath (id, arrayOfPoints) {
+  var curveEntity = document.createElement('a-curve');
+	var curvepoint, i, point;
+
+	curveEntity.setAttribute('id', id);
+  for (i = 0; i < arrayOfPoints.length; i = i + 1) {
+		point = arrayOfPoints[i];
+    curvePoint = document.createElement('a-curve-point');
+    curvePoint.setAttribute('position', `${point.x} ${point.y} ${point.z}`);
+    curveEntity.appendChild(curvePoint);
+  }
+  return curveEntity;
+}
+
 
 AFRAME.registerComponent('gamelogic', {
 	schema: { // what goes into the component
@@ -41,26 +78,28 @@ AFRAME.registerComponent('gamelogic', {
 		this.state = this.data.state;
 		console.log("state: " + this.state);
 		this.avatar.setAttribute('sound', "src: #start; autoplay: true");
-		// 1. start of the scene, we follow the rabbit
+
+		// 1. we follow the rabbit
 		if (this.state === this.states.follow) {
 			this.player.setAttribute('sound', "src: #select; autoplay: true");
 			this.followme.setAttribute('visible', false);
 			this.player.setAttribute('follow', 'target', '#avatar');
 			this.rabbit.setAttribute('animation__hop', 'property: position; to: 0 0.3 0; dur: 250; easing: easeInOutSine; loop: true');
 			this.rabbit.removeAttribute('event-set__follow');
-			this.avatar.setAttribute('alongpath', 'curve: '+this.track+'; dur: 3000');
-			this.avatar.setAttribute('event-set__stopfollow', '_event: movingended; _target: #gamelogic; gamelogic.state: ' + this.states.stopfollow);
+      this.avatar.setAttribute('move-along', 'dur: 3000; points: 0 72.5 6.15, 0 72.5 4.15, 0 72.5 0.15, 0 72.5 -4.15, 0 72.5 -12.15, 0 74 -18.15;');
+      this.avatar.setAttribute('event-set__stopfollow', '_event: move-along-end; _target: #gamelogic; gamelogic.state: ' + this.states.stopfollow);
+
 		// 2. the rabbit has reached its destination hovering in mid-air
 		} else if (this.state === this.states.stopfollow) {
 			this.player.setAttribute('sound', 'src: #fall; autoplay: true');
 			this.player.removeAttribute('follow');
-			this.avatar.removeAttribute('alongpath');
+			this.avatar.removeAttribute('move-along');
 			this.avatar.removeAttribute('event-set__stopfollow');
 			this.rabbit.removeAttribute('animation__hop');
-			this.track = '#track2';
-			this.avatar.setAttribute('alongpath', 'curve: '+this.track+'; delay: 2500; dur: 3000');
-			this.avatar.setAttribute('event-set__rabbithasfallen', '_event: movingended; _target: #gamelogic; gamelogic.state: ' + this.states.rabbithasfallen);
-			// 3. the rabbit has fallen :( the chicken rises
+      this.avatar.setAttribute('move-along', 'delay: 2500; dur: 3000; points: 0 74 -18.15, 0 62 -18, 0 31 -18.15, 0 -20 -17;');
+      this.avatar.setAttribute('event-set__rabbithasfallen', '_event: move-along-end; _target: #gamelogic; gamelogic.state: ' + this.states.rabbithasfallen);
+
+		// 3. the rabbit has fallen :( the chicken rises
 		} else if (this.state === this.states.rabbithasfallen) {
 			this.avatar.components.sound.stopSound();
 			this.avatar.removeAttribute('sound');
@@ -71,7 +110,8 @@ AFRAME.registerComponent('gamelogic', {
 			this.chicken.setAttribute('animation__pos', 'property: position; dur: 14000; easing: easeInSine; to: 0 0 -550');
 			this.chicken.setAttribute('animation__rot', 'property: rotation; dur: 14000; easing: easeInSine; to: 0 -17 0');
 			this.chicken.setAttribute('event-set__chickenhasrisen', '_event: animation__rot-complete; _target: #gamelogic; gamelogic.state: ' + this.states.chickenhasrisen);
-			// 4. how to deal with the threatening chicken - we have two choices
+
+		// 4. how to deal with the threatening chicken - player has two choices
 		} else if (this.state === this.states.chickenhasrisen) {
 			this.chicken.removeAttribute('animation__pos');
 			this.chicken.removeAttribute('animation__rot');
@@ -87,15 +127,18 @@ AFRAME.registerComponent('gamelogic', {
 				this.el.setAttribute('gamelogic', 'state: ' + this.states.heartselected);
 			}).bind(this));
 			raycaster.refreshObjects();
+
+		// 5. player has made a choice
 		} else if (this.state === this.states.ak47selected || this.state === this.states.heartselected) {
 			this.player.setAttribute('sound', "src: #select; autoplay: true");
+
 			if (this.state === this.states.ak47selected) {
-				// violence begins
 				target = this.platform3.getAttribute('position');
 				parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: ' + target.x + ' ' + (target.y + 33) + ' ' + target.z;
 				this.player.removeAttribute('animation');
 				this.player.setAttribute('animation__toak47', parameters);
 				this.player.setAttribute('event-set__playeratak47', '_event: animation__toak47-complete; _target: #gamelogic; gamelogic.state: ' + this.states.atak47);
+
 			} else if (this.state === this.states.heartselected) {
 				target = this.platform2.getAttribute('position');
 				parameters = 'property: position; dur: 2000; easing: easeInOutQuad; to: '+target.x+' '+(target.y + 36)+' '+target.z;
@@ -103,12 +146,14 @@ AFRAME.registerComponent('gamelogic', {
 				this.player.setAttribute('animation__toheart', parameters);
 				this.player.setAttribute('event-set__playeratheart', '_event: animation__toheart-complete; _target: #gamelogic; gamelogic.state: ' + this.states.atheart);
 			}
-			console.log(target);
+
 			// remove all choice elements
 			this.heart.parentNode.removeChild(this.heart);
 			this.ak47.parentNode.removeChild(this.ak47);
 			this.killthebeast.parentNode.removeChild(this.killthebeast);
 			this.loveandpeace.parentNode.removeChild(this.loveandpeace);
+
+		// 6. player has arrived at final destination
 		} else if (this.state === this.states.atak47 || this.state === this.states.atheart) {
 			score = 0;
 			this.chicken.setAttribute('class', 'interactive');
@@ -125,12 +170,13 @@ AFRAME.registerComponent('gamelogic', {
 			} else if (this.state === this.states.atheart) {
 				this.player.setAttribute('spawner', { 'mixin': 'heartbullet', 'on': 'click' });	// TODO change trigger
 			}
+
+		// 7. let it rain
 		} else if (this.state === this.states.letitrain) {
 			this.player.components.sound.stopSound();
 			this.player.removeAttribute('sound');
 			this.player.setAttribute('sound', "src: #ending; autoplay: true");
 			playerPosition = this.player.getAttribute('position');
-			console.log(playerPosition.x + ', ' + playerPosition.y + ', ' + playerPosition.z);
 			this.scene.removeEventListener('click', increaseCounter);
 			this.scene.setAttribute('rain-of-chickens', { tagName: 'a-sphere', center: {x: playerPosition.x, y: (playerPosition.y + 30), z: playerPosition.z} });
 		}
---
+++ 06_rabbit_chicken_ak47/js/aframe-move-along-component.js
@@ -0,0 +1,103 @@
+/* global AFRAME */
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+/**
+ * Minimal Move-along Component for A-Frame.
+ */
+AFRAME.registerComponent('move-along', {
+  schema: {
+    delay: { default: 0 },
+    dur: { default: 1000 },
+    loop: { default: false },
+    on: { default: '' },
+    points: {
+      default: [],
+      parse: function (value) {
+        var arr = value.trim().split(',');
+        var result = [];
+        var i;
+
+        for (i = 0; i < arr.length; i = i + 1) {
+          result.push(AFRAME.utils.coordinates.parse(arr[i]));
+        }
+        return result;
+      }
+    }
+  },
+
+  init: function () {
+    this.startMoveAlong = (function () {
+      var points = this.data.points;
+      var threePoints = [];
+      var i;
+      this.elapsed = 0;
+      this.animate = true;
+      for (i = 0; i < points.length; i = i + 1) {
+        threePoints.push(new THREE.Vector3(points[i].x, points[i].y, points[i].z));
+      }
+      this.threeConstructor = THREE['CatmullRomCurve3'];
+      this.curve = new this.threeConstructor(threePoints);
+    }).bind(this);
+  },
+
+  update: function (oldData) {
+    var data = this.data;
+    var el = this.el;
+
+    // if the event handler has been changed set the new one
+    if (oldData.on != null && data.on !== oldData.on) {
+      el.removeEventListener(oldData.on, this.startMoveAlong);
+    }
+    if (data.on != null && data.on !== '') {
+      el.addEventListener(data.on, this.startMoveAlong);
+    } else { // directly start to move
+      this.startMoveAlong();
+    }
+  },
+
+  tick: function (time, delta) {
+    var el = this.el;
+    var curve = this.curve;
+    var delay = this.data.delay;
+    var duration = this.data.dur;
+    var loop = this.data.loop;
+    var point, ratio;
+
+    if (this.animate === false) { return; }
+    this.elapsed = this.elapsed + delta;
+    if (this.elapsed < delay) { return; }
+    if (this.elapsed > delay + duration) {
+      el.setAttribute('position', curve.points[curve.points.length - 1]);
+      el.emit('move-along-end');
+      if (loop === false) {
+        this.animate = false;
+      } else {
+        this.elapsed = 0;
+      }
+      return;
+    }
+    ratio = (this.elapsed - delay) / duration;
+    point = curve.getPointAt(ratio);
+    el.setAttribute('position', point);
+  },
+
+  pause: function () {
+  },
+
+  play: function () {
+  },
+
+  remove: function () {
+    var data = this.data;
+    var el = this.el;
+    this.animate = false;
+
+    if (data.on != null) {
+      el.removeEventListener(data.on, this.eventHandlerFn);
+    }
+  }
+
+});
